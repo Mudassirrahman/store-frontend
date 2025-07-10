9c9d7b140c88557d2d92b11f6a3170df63ad7fb4
mudassirrahman <mudassirrehman1989@gmail.com>
2025-07-10 14:37:05 +0500

    custom css added for paginator

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import App from './App.jsx'
 import 'primereact/resources/themes/lara-light-blue/theme.css';  // Theme
 import 'primereact/resources/primereact.min.css';                // Core CSS
 import 'primeicons/primeicons.css';                              // Icons
-import 'primeflex/primeflex.css';
+import 'primeflex/primeflex.css';                                // CSS
+import "./App.css";
 
 
 
